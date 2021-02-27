8f19e3651f9fb049f4b813ee82846cced29fb017
Leo Natan <leo.natan@outlook.com>
2021-02-27 14:19:03 +0200

    Apple Simulator Utils 1.0

--- applesimutils.rb
+++ applesimutils.rb
@@ -1,20 +1,20 @@
 class Applesimutils < Formula
   desc 'Apple Simulator Utilities'
   homepage 'https://github.com/LeoNatan/AppleSimulatorUtils'
-  url 'https://github.com/LeoNatan/AppleSimulatorUtils/releases/download/0.9.2/AppleSimulatorUtils-0.9.2.tar.gz'
-  sha256 'aa14267ecf5ad09904047fef3dbfa4ddf05c99b859771380d01de3d911bcb9d8'
+  url 'https://github.com/LeoNatan/AppleSimulatorUtils/releases/download/1.0/AppleSimulatorUtils-1.0.tar.gz'
+  sha256 '6637b2a4cb2bd25639380d919b5bf788a31fa5ba5e7642819d716ff29b20034f'
   head 'https://github.com/LeoNatan/AppleSimulatorUtils.git'
 
   depends_on xcode: ['8.0', :build]
   
   bottle do
-    root_url 'https://github.com/LeoNatan/AppleSimulatorUtils/releases/download/0.9.2'
-    sha256 '11c57fb0008b59552881f03d6897de54ad4e7046cb19ff26725ce34d3a6c1738' => :catalina
-    sha256 '11c57fb0008b59552881f03d6897de54ad4e7046cb19ff26725ce34d3a6c1738' => :mojave
-    sha256 '11c57fb0008b59552881f03d6897de54ad4e7046cb19ff26725ce34d3a6c1738' => :high_sierra
-    sha256 '11c57fb0008b59552881f03d6897de54ad4e7046cb19ff26725ce34d3a6c1738' => :sierra
-    sha256 '11c57fb0008b59552881f03d6897de54ad4e7046cb19ff26725ce34d3a6c1738' => :big_sur
-    sha256 '11c57fb0008b59552881f03d6897de54ad4e7046cb19ff26725ce34d3a6c1738' => :arm64_big_sur
+    root_url 'https://github.com/LeoNatan/AppleSimulatorUtils/releases/download/1.0'
+    sha256 'ef3bfb0106aa8e6beaef15eb619a901ca401e9e1d94a8aea154a2116a370e465' => :catalina
+    sha256 'ef3bfb0106aa8e6beaef15eb619a901ca401e9e1d94a8aea154a2116a370e465' => :mojave
+    sha256 'ef3bfb0106aa8e6beaef15eb619a901ca401e9e1d94a8aea154a2116a370e465' => :high_sierra
+    sha256 'ef3bfb0106aa8e6beaef15eb619a901ca401e9e1d94a8aea154a2116a370e465' => :sierra
+    sha256 'ef3bfb0106aa8e6beaef15eb619a901ca401e9e1d94a8aea154a2116a370e465' => :big_sur
+    sha256 'ef3bfb0106aa8e6beaef15eb619a901ca401e9e1d94a8aea154a2116a370e465' => :arm64_big_sur
   end
 
   def install
