1e2c0b54b67f06bc9b3d86f66fdd7ce03aefc81b
Leo Natan <leo.natan@outlook.com>
2021-04-23 18:39:27 +0300

    Update formula syntax

--- applesimutils.rb
+++ applesimutils.rb
@@ -5,16 +5,16 @@ class Applesimutils < Formula
   sha256 '15564fdfcf06417a4b1dc4b5723354cafd8fd3542736e411291e7b575905de9a'
   head 'https://github.com/LeoNatan/AppleSimulatorUtils.git'
 
-  depends_on xcode: ['8.0', :build]
+  depends_on xcode: ['12.0', :build]
   
   bottle do
     root_url 'https://github.com/LeoNatan/AppleSimulatorUtils/releases/download/1.1'
-    sha256 '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c' => :catalina
-    sha256 '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c' => :mojave
-    sha256 '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c' => :high_sierra
-    sha256 '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c' => :sierra
-    sha256 '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c' => :big_sur
-    sha256 '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c' => :arm64_big_sur
+    sha256 catalina: '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c'
+    sha256 mojave: '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c'
+    sha256 high_sierra: '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c'
+    sha256 sierra: '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c'
+    sha256 big_sur: '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c'
+    sha256 arm64_big_sur: '2387ac3c1b4604504686f43ce3c403af6dae93d518f001c96a68cf738fa1358c'
   end
 
   def install
