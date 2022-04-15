8848984924fcd663f2f136b0c0d2cd2b051da04f
Leo Natan <leo.natan@outlook.com>
2022-04-15 15:48:36 +0300

    Change address

--- Casks/detox-instruments.rb
+++ Casks/detox-instruments.rb
@@ -4,7 +4,7 @@ cask 'detox-instruments' do
 
   url 'https://github.com/wix/DetoxInstruments/releases/download/1.15.13062/DetoxInstruments-v1.15.b13062.zip'
   name 'Detox Instruments'
-  homepage 'https://github.com/wix/DetoxInstruments'
+  homepage 'https://github.com/LeoNatan/DetoxInstruments'
 
   depends_on macos: '>= :catalina'
   
